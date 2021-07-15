ed1bd5d49630578bdfe56ea171b88c17e6eb78ad
turgonboyev <85232464+turgonboyev@users.noreply.github.com>
2021-07-15 09:40:01 +0500

    Create sort().py

---
+++ Kirish/sort().py
@@ -0,0 +1,18 @@
+
+# Sort funksiyasini ishlatish
+
+a = ["HTML", "AdobePhotoshop", "Python", "css", "Sass", "JavaScript"]
+
+a.sort()
+
+print("Tartiblangan ro'yxat :")
+
+print(a)
+
+# Tartibni oxiridan boshlash
+
+a.sort(reverse=True)
+
+print("Teskari tartiblangan ro'yxat : ")
+
+print(a)
